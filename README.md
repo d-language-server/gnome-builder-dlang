677520f07f34cfe6910434a641400145004f6761
Laurent Tréguier <laurent@treguier.org>
2019-01-19 19:32:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# D support for [GNOME Builder](https://wiki.gnome.org/Apps/Builder)
+
+A GNOME Builder plugin for [Dlang](https://dlang.org).
+Provides dub project and build integration, and editing features using the [Language Server protocol](https://microsoft.github.io/language-server-protocol).
+
+## Features
+
+[DLS](https://github.com/d-language-server/dls) is used as a Language Server, which in turn uses libraries such as [DCD](http://dcd.dub.pm), [DFMT](http://dfmt.dub.pm), [D-Scanner](http://dscanner.dub.pm) as well as [other libraries](https://github.com/d-language-server/dls/blob/master/README.md) to provide language editing features.
+
+Look [here](https://github.com/d-language-server/dls) for an up-to-date list of features currently supported.
+Not every possible feature is implemented, but the server will update itself as new features come.
+
+## Requirements
+
+Dub and either DMD or LDC should be installed for the extension to work properly.
---
+++ dlang_langserver_plugin.py
@@ -0,0 +1,106 @@
+import gi
+import os
+
+from gi.repository import GLib
+from gi.repository import Gio
+from gi.repository import GObject
+from gi.repository import Ide
+
+
+class DlangService(Ide.Object, Ide.Service):
+    _client = None
+    _has_started = False
+
+    @GObject.Property(type=Ide.LangservClient)
+    def client(self):
+        return self._client
+
+    @client.setter
+    def client(self, value):
+        self._client = value
+        self.notify("client")
+
+    def do_stop(self):
+        if self._client is not None:
+            client, self._client = self._client, None
+            client.stop()
+
+    def _ensure_started(self):
+        if self._has_started:
+            return
+
+        self._has_started = True
+        launcher = self._create_launcher()
+        launcher.set_clear_env(False)
+
+        workdir = self.get_context().get_vcs().get_working_directory()
+        launcher.set_cwd(workdir.get_path())
+        launcher.push_argv(os.path.join(GLib.get_home_dir(), ".dub", "packages", ".bin", "dls-latest", "dls"))
+        launcher.push_argv("--stdio")
+
+        supervisor = Ide.SubprocessSupervisor()
+        supervisor.connect("spawned", self._dls_spawned)
+        supervisor.set_launcher(launcher)
+        supervisor.start()
+
+    def _dls_spawned(self, supervisor, subprocess):
+        stdin = subprocess.get_stdin_pipe()
+        stdout = subprocess.get_stdout_pipe()
+        io_stream = Gio.SimpleIOStream.new(stdout, stdin)
+
+        if self._client:
+            self._client.stop()
+
+        self._client = Ide.LangservClient.new(self.get_context(), io_stream)
+        self._client.add_language("d")
+        self._client.start()
+        self.notify("client")
+
+    def _create_launcher(self):
+        flags = Gio.SubprocessFlags.STDIN_PIPE | Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_SILENCE
+        launcher = Ide.SubprocessLauncher()
+        launcher.set_flags(flags)
+        launcher.set_cwd(GLib.get_home_dir())
+        launcher.set_run_on_host(True)
+        return launcher
+
+    @classmethod
+    def bind_client(klass, provider):
+        context = provider.get_context()
+        self = context.get_service_typed(DlangService)
+        self._ensure_started()
+        self.bind_property("client", provider, "client", GObject.BindingFlags.SYNC_CREATE)
+
+class DlangDiagnosticProvider(Ide.LangservDiagnosticProvider):
+    def do_load(self):
+        DlangService.bind_client(self)
+
+class DlangCompletionProvider(Ide.LangservCompletionProvider):
+    def do_load(self, context):
+        DlangService.bind_client(self)
+
+    def do_get_priority(self, context):
+        return -1000
+
+class DlangRenameProvider(Ide.LangservRenameProvider):
+    def do_load(self):
+        DlangService.bind_client(self)
+
+class DlangSymbolResolver(Ide.LangservSymbolResolver):
+    def do_load(self):
+        DlangService.bind_client(self)
+
+class DlangHighlighter(Ide.LangservHighlighter):
+    def do_load(self):
+        DlangService.bind_client(self)
+
+class DlangFormatter(Ide.LangservFormatter):
+    def do_load(self):
+        DlangService.bind_client(self)
+
+class DlangHoverProvider(Ide.LangservHoverProvider):
+    def do_prepare(self):
+        self.props.category = "D"
+
+    def do_load(self, context):
+        DlangService.bind_client(self)
