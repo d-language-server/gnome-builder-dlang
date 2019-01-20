73f73b4c1be486c6e06c3b4792fc194975036079
Laurent Tréguier <laurent@treguier.org>
2019-01-20 12:51:26 +0100

    Send dub.selections.json changes

--- dlang_langserver_plugin.py
+++ dlang_langserver_plugin.py
@@ -10,6 +10,7 @@ from gi.repository import Ide
 class DlangService(Ide.Object, Ide.Service):
     _client = None
     _has_started = False
+    _monitor = None
 
     @GObject.Property(type=Ide.LangservClient)
     def client(self):
@@ -20,7 +21,26 @@ class DlangService(Ide.Object, Ide.Service):
         self._client = value
         self.notify("client")
 
+    def do_context_loaded(self):
+        context = self.get_context()
+        project_file = context.get_project_file()
+        project_dir = project_file if project_file.query_file_type(0, None) == Gio.FileType.DIRECTORY else project_file.get_parent()
+        selections_file = project_dir.get_child("dub.selections.json")
+        self._monitor = selections_file.monitor(0, None)
+        self._monitor.set_rate_limit(1000)
+        self._monitor.connect("changed", self._monitor_changed_cb)
+
+    def _monitor_changed_cb(self, monitor, file, other_file, event_type):
+        if self._client and file.get_basename() == "dub.selections.json":
+            file_event = GLib.Variant("a{sv}", { "uri": GLib.Variant.new_string(file.get_uri()), "type": GLib.Variant.new_byte(2) })
+            files_params = GLib.Variant("a{sav}", { "changes": [file_event] })
+            self._client.send_notification_async("workspace/didChangeWatchedFiles", files_params, None, self._dls_notification_finish, None)
+
     def do_stop(self):
+        if self._monitor is not None:
+            monitor, self._monitor = self._monitor, None
+            monitor.cancel()
+
         if self._client is not None:
             client, self._client = self._client, None
             client.stop()
@@ -63,7 +83,8 @@ class DlangService(Ide.Object, Ide.Service):
         self.notify("client")
 
     def _dls_notification_finish(self, source_object, result, user_data):
-        self._client.send_notification_finish(result)
+        if self._client:
+            self._client.send_notification_finish(result)
 
     def _create_launcher(self):
         flags = Gio.SubprocessFlags.STDIN_PIPE | Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_SILENCE
